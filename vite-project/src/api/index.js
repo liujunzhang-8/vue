4bf984ecb582848dd9d49e51eb38836af8d631d2
Gorgio_Liu666 <15711097909@163.com>
2022-05-19 17:20:00 +0800

    login

---
+++ vite-project/src/api/index.js
@@ -0,0 +1,9 @@
+import axios from "@/utils/request";
+
+export function Login(data) {
+    return axios({
+        method: 'post',
+        url: `get_login`,
+        data
+    })
+}
---
+++ vite-project/src/utils/request.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const service = axios.create({
+    baseURL: "http://127.0.0.1:3333/"
+})
+
+service.interceptors.request.use(config => config)
+service.interceptors.response.use(response => {
+    return response
+}, err => {
+    return Promise.reject(err);
+})
+
+export default service;
