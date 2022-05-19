d52ca44fb6dd0196339aac526272711055b9e1fa
Gorgio_Liu666 <15711097909@163.com>
2022-05-19 15:55:07 +0800

    config vite

--- vite-project/src/components/HelloWorld.vue
+++ vite-project/src/components/HelloWorld.vue
@@ -1,36 +1,35 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 
 defineProps({
   msg: String
 })
+
+const state = reactive({
+    id: 100,
+    arr: [1,2,3],
+    obj: {id: 200}
+})
+
+const change = () => {
+    count.value++
+}
 
 const count = ref(0)
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
   <p>
     <a href="https://vitejs.dev/guide/features.html" target="_blank">
       Vite Documentation
     </a>
-    |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
   </p>
 
   <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <button @click="change()">响应式：{{state.id}}</button>
+  <div>{{state.id}}</div>
 </template>
 
 <style scoped>
