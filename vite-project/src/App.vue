1cc545cb0c3e95bb89e6d8238fa49bc66f589c48
Gorgio_Liu666 <15711097909@163.com>
2022-05-22 19:10:20 +0800

    接口api配置

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,131 +1,194 @@
 <script setup>
-import { reactive } from 'vue'
-import Header from '@/components/Header.vue'
-import Footer from '@/components/Footer.vue'
-import { useRouter } from 'vue-router'
-import {pathMap, localGet } from '@/utils'
+import { reactive } from "vue";
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+import { useRouter } from "vue-router";
+import { pathMap, localGet } from "@/utils";
+import {
+  TrendCharts,
+  HomeFilled,
+  CirclePlusFilled,
+  PictureFilled,
+  StarFilled,
+  SoldOut,
+  Pointer,
+  Menu,
+  GoodsFilled,
+  Avatar,
+  Document,
+  Lock,
+} from "@element-plus/icons-vue";
 components: {
-    Header,
-    Footer
+  Header, Footer;
 }
 const state = reactive({
-    defaultOpen: ['1'],
-    showMenu: true,
-    currentPath: '/home',
-    count: {
-        number: 1
-    }
-})
+  defaultOpen: ["1"],
+  showMenu: true,
+  currentPath: "/home",
+  count: {
+    number: 1,
+  },
+});
 </script>
 
 <template>
   <div class="layout">
-      <el-container v-if="state.showMenu" class="container">
-          <el-aside class="aside">
-              <div class="header">
-                      <img src="./assets/logo.png" alt="">
-                      <span>vue3 admin</span>
-              </div>
-              <el-menu :default-openeds="state.defaultOpen" background-color="#222832" text-color="#fff" :router="true" :default-active="state.currentPath">
-                <el-sub-menu index="1">
-                    <template #title>
-                        <span>Home</span>
-                    </template>
-                </el-sub-menu>
-                <el-sub-menu index="2">
-                    <template #title>
-                        <span>首页配置</span>
-                    </template>
-                </el-sub-menu>
-                <el-sub-menu index="3">
-                    <template #title>
-                        <span>模块管理</span>
-                    </template>
-                </el-sub-menu>
-                <el-sub-menu index="4">
-                    <template #title>
-                        <span>系统管理</span>
-                    </template>
-                </el-sub-menu>
-              </el-menu>
-          </el-aside>
-          <el-container class="content">
-              <Header />
-              <div class="main">
-                  <router-view></router-view>
-              </div>
-              <Footer />
-          </el-container>
-      </el-container>
-      <el-container v-else class="container">
+    <el-container v-if="state.showMenu" class="container">
+      <el-aside class="aside">
+        <div class="header">
+          <img src="./assets/logo.png" alt="" />
+          <span>vue3 admin</span>
+        </div>
+        <el-menu
+          :default-openeds="state.defaultOpen"
+          background-color="#222832"
+          text-color="#fff"
+          :router="true"
+          :default-active="state.currentPath"
+        >
+          <el-sub-menu index="1">
+            <template #title>
+              <span>Home</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item
+                ><el-icon><TrendCharts /></el-icon>系统介绍</el-menu-item
+              >
+              <el-menu-item
+                ><el-icon><HomeFilled /></el-icon>Home</el-menu-item
+              >
+              <el-menu-item
+                ><el-icon><CirclePlusFilled /></el-icon>添加商品</el-menu-item
+              >
+            </el-menu-item-group>
+          </el-sub-menu>
+          <el-sub-menu index="2">
+            <template #title>
+              <span>首页配置</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item
+                ><el-icon><PictureFilled /></el-icon>轮播图配置</el-menu-item
+              >
+              <el-menu-item
+                ><el-icon><StarFilled /></el-icon>热销商品配置</el-menu-item
+              >
+              <el-menu-item
+                ><el-icon><SoldOut /></el-icon>新品上线配置</el-menu-item
+              >
+              <el-menu-item
+                ><el-icon><Pointer /></el-icon>为你推荐配置</el-menu-item
+              >
+            </el-menu-item-group>
+          </el-sub-menu>
+          <el-sub-menu index="3">
+            <template #title>
+              <span>模块管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item
+                ><el-icon><Menu /></el-icon>分类管理</el-menu-item
+              >
+              <el-menu-item
+                ><el-icon><GoodsFilled /></el-icon>商品管理</el-menu-item
+              >
+              <el-menu-item
+                ><el-icon><Avatar /></el-icon>会员管理</el-menu-item
+              >
+              <el-menu-item
+                ><el-icon><Document /></el-icon>订单管理</el-menu-item
+              >
+            </el-menu-item-group>
+          </el-sub-menu>
+          <el-sub-menu index="4">
+            <template #title>
+              <span>系统管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item
+                ><el-icon><Lock /></el-icon>修改密码</el-menu-item
+              >
+            </el-menu-item-group>
+          </el-sub-menu>
+        </el-menu>
+      </el-aside>
+      <el-container class="content">
+        <Header />
+        <div class="main">
           <router-view></router-view>
+        </div>
+        <Footer />
       </el-container>
+    </el-container>
+    <el-container v-else class="container">
+      <router-view></router-view>
+    </el-container>
   </div>
 </template>
 
 <style lang="scss" scoped>
 body {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
 .layout {
-    min-height: 100vh;
-    background-color: #fff;    
-    .container {
-            height: 100vh;
-            .aside {
-                width: 200px;
-                background-color: #222832;
-                overflow: hidden;
-                overflow-y: auto;
-                -ms-overflow-style: none;
-                overflow: -moz-scrollbars-none;
-                .header {
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                    height: 50px;
-                    border-bottom: 1px solid hsla(0, 0%, 100%, .2);
-                    img {
-                        width: 30px;
-                        height: 30px;
-                        margin-right: 10px;
-                    }
-                    span {
-                        font-size: 20px;
-                        color: #fff;
-                    }
-                }
-            }
-            .aside::-webkit-scrollbar {
-                display: none;
-            }
-            .content {
-                display: flex;
-                flex-direction: column;
-                max-height: 100vh;
-                overflow: hidden;
-                .main {
-                    height: calc(100vh - 100px);
-                    overflow: auto;
-                    padding: 10px;
-                }
-            }
+  min-height: 100vh;
+  background-color: #fff;
+  .container {
+    height: 100vh;
+    .aside {
+      width: 200px;
+      background-color: #222832;
+      overflow: hidden;
+      overflow-y: auto;
+      -ms-overflow-style: none;
+      overflow: -moz-scrollbars-none;
+      .header {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 50px;
+        border-bottom: 1px solid hsla(0, 0%, 100%, 0.2);
+        img {
+          width: 30px;
+          height: 30px;
+          margin-right: 10px;
         }
+        span {
+          font-size: 20px;
+          color: #fff;
+        }
+      }
+    }
+    .aside::-webkit-scrollbar {
+      display: none;
+    }
+    .content {
+      display: flex;
+      flex-direction: column;
+      max-height: 100vh;
+      overflow: hidden;
+      .main {
+        height: calc(100vh - 100px);
+        overflow: auto;
+        padding: 10px;
+      }
+    }
+  }
 }
 .el-menu {
-    border-right: none;
-    .el-sub-menu {
-        border-top: 1px solid hsla(0, 0%, 100%, .05);
-        border-bottom: 1px solid rgba($color: #000, $alpha: .2);
-        .el-sub-menu [class^="el-icon-"] {
-            vertical-align: -1px;
-        }
-        a {
-            color: #409eff;
-            text-decoration: none;
-        }
+  border-right: none;
+  .el-sub-menu {
+    border-top: 1px solid hsla(0, 0%, 100%, 0.05);
+    border-bottom: 1px solid rgba($color: #000, $alpha: 0.2);
+    .el-sub-menu [class^="el-icon-"] {
+      vertical-align: -1px;
     }
+    a {
+      color: #409eff;
+      text-decoration: none;
+    }
+  }
 }
 </style>
--- vite-project/src/view/Login/index.vue
+++ vite-project/src/view/Login/index.vue
@@ -1,38 +1,51 @@
 <template>
   <div class="login">
-    <el-form
-      ref="ruleFormRef"
-      :model="temp"
-      :rules="rules"
-      label-width="120px"
-      class="demo-ruleForm"
-    >
-      <el-form-item label="用户名" prop="user">
-        <el-input v-model="temp.user" />
-      </el-form-item>
-      <el-form-item label="密码" prop="password">
-        <el-input type="password" v-model="temp.password" />
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="login(ruleFormRef)">登录</el-button>
-      </el-form-item>
-    </el-form>
+    <div class="login-container">
+      <div class="head">
+          <img src="@/assets/logo.png" alt="" />
+          <span>vue3 admin</span>
+      </div>
+      <el-form
+        ref="ruleFormRef"
+        label-position="top"
+        :model="temp"
+        :rules="rules"
+        class="login-form"
+      >
+        <el-form-item label="账号" prop="username">
+          <el-input type="text" v-model.trim="temp.username" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input type="password" v-model.trim="temp.password" autocomplete="off" />
+        </el-form-item>
+        <el-form-item>
+            <div style="color: #333;">登录表示您已同意<a href="">《服务条款》</a></div>
+          <el-button style="width: 100%;" type="primary" @click="submitForm">立即登录</el-button>
+          <el-checkbox v-model="state.checked" @change="!state.checked">下次自动登录</el-checkbox>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { ref, reactive } from "vue";
-import {useRouter} from 'vue-router'
-import {Login} from '../../api/index'
+import axios from "@/utils/axios.js";
+import { localSet } from '@/utils'
+import md5 from 'js-md5'
 
-const router = useRouter() //实例化
-const ruleFormRef = ref();
+const ruleFormRef = ref(null);
 const temp = reactive({
   password: "",
-  user: "",
+  username: "",
 });
+
+const state = reactive({
+    checked: true
+})
+
 const rules = reactive({
-  user: [
+  username: [
     { required: true, message: "请输入用户名", trigger: "blur" },
     { min: 3, max: 18, message: "长度在3~18个字符", trigger: "blur" },
   ],
@@ -42,22 +55,57 @@ const rules = reactive({
   ],
 });
 // 登录函数
-function login(temp) {
+const submitForm = async () => {
     ruleFormRef.value.validate(valid => {
         if(valid) {
-            Login().then(res => {
-                let {code, message} = res.data
-                if(code == 200) {
-                    // 跳转
-                    router.push('/home')
-                } else {
-                    alert(message)
-                }
+            console.log('校验通过');
+            axios.post('/adminUser/login', {
+                username: temp.username || '',
+                passwordMd5: md5(temp.password)
+            }).then (res => {
+                console.log('秦穆公求成');
             })
         } else {
-            console.log('error submit!');
+            console.log('error submit!!!');
+            return false
         }
     })
-    
 }
 </script>
+
+<style lang="scss" scoped>
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  background: #fff;
+  .login-container {
+    width: 420px;
+    height: 500px;
+    background-color: #fff;
+    border-radius: 4px;
+    box-shadow: 0px 21px 41px 0px rgba($color: #000000, $alpha: 0.2);
+    .head {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 40px 0 20px 0;
+        img {
+            width: 60px;
+            height: 60px;
+            margin-right: 20px;
+        }
+        span {
+            font-size: 28px;
+            color: #1BAEAE;
+            font-weight: bold;
+        }
+    }
+    .login-form {
+        width: 70%;
+        margin: 0 auto;
+    }
+  }
+}
+</style>
