229b810deb83d4a573301e9010d5a5816b3e4452
Gorgio_Liu666 <15711097909@163.com>
2022-05-22 14:27:52 +0800

    axios

--- vite-project/src/utils/axios.js
+++ vite-project/src/utils/axios.js
@@ -1,14 +1,32 @@
 import axios from 'axios';
+import router from '@/router';
+import { localGet } from './index';
+import config from '~/config';
+import { ElMessage } from 'element-plus'
 
-const service = axios.create({
-    baseURL: "http://127.0.0.1:4523/mock/1006727/"
+axios.defaults.baseURL = config[import.meta.env.MODE].baseURL
+// token 鉴权
+axios.defaults.withCredentials = true
+// 请求头，headers 信息
+axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest'
+axios.defaults.headers['token'] = localGet('token') || ''
+// 默认 post 请求，使用 application/json 形式
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+
+// 请求拦截器，内部根据返回值，重新组装，统一管理
+axios.interceptors.response.use(res => {
+    if(typeof res.data !== 'object') {
+        ElMessage.error('服务端异常！')
+        return Promise.reject(res)
+    }
+    if(res.data.resultCode != 200) {
+        if (res.data.message) ElMessage.error(res.data.message)
+        if(res.data.resultCode == 419) {
+            router.push({path: '/login'})
+        }
+        return Promise.reject(res.data)
+    }
+    return res.data.data
 })
 
-service.interceptors.request.use(config => config)
-service.interceptors.response.use(response => {
-    return response
-}, err => {
-    return Promise.reject(err);
-})
-
-export default service;
+export default axios;
