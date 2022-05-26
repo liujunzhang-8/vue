d91ffa270f6655f93a5b27922d78b3a5804e67cf
Gorgio_Liu666 <15711097909@163.com>
2022-05-26 14:23:02 +0800

    feat: swiper

--- vite-project/src/view/swiper/index.vue
+++ vite-project/src/view/swiper/index.vue
@@ -1,11 +1,26 @@
 <template>
-    <div class='swiper'>
-        <h1>轮播图配置</h1>
-    </div>
+    <el-card class='swiper-container'>
+         <template #header>
+             <div class="header">
+                 <el-button type="primary" size="small" :icon="Plus">增加</el-button>
+                 <el-popconfirm title="确定删除吗?">
+                     <template #reference>
+                         <el-button type="danger" size="small" :icon="Delete">批量删除</el-button>
+                     </template>
+                 </el-popconfirm>
+             </div>
+         </template>
+         <el-table></el-table>
+    </el-card>
 </template>
 
 <script setup>
+import { reactive } from 'vue';
+import { Plus, Delete } from "@element-plus/icons-vue";
+
 </script>
 <style lang='scss' scoped>
-
+.swiper-container {
+    min-height: 100%;
+}
 </style>
